843d5e587688cc8cfcfb58df08ec34c7971fcb5b
HongBo0531 <zhbsimon@163.com>
2022-04-18 17:03:05 +0800

    init leancloud

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-createApp(App).use(router).mount('#app')
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+const AV = require("leancloud-storage");
+AV.init({
+  appId: "dm8VxvGH6qok0dks0xtyx3RD-gzGzoHsz",
+  appKey: "qtgvVFBqCvQKQa3x0pqUevgL",
+  serverURL: "https://please-replace-with-your-customized.domain.com"
+});
+createApp(App).use(router).mount("#app");
